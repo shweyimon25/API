25a110a6e2f6bf0a5b04bf4fd602aed4ddb58cdf
DevStream <devstream.mm@gmail.com>
2025-07-23 09:21:48 +0630

    Create User, Role & Permission

--- dist/app/middlewares/logger.middleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const loggerMiddleware = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
-exports.default = loggerMiddleware;
